4d67e268ca9965f5560414605e9d1e3b1e6bfdf6
ashwin <ashwin@pop-os.localdomain>
2021-07-21 09:39:08 +0530

    Build sucessfull

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+	  Welcome to Srist Space 1.0
+	</p>
+	<p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
