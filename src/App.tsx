4c573d8a230c89b38249fc5a4c9a995f6cac14c8
ashwin <ashwin@pop-os.localdomain>
2021-07-23 12:23:40 +0530

    carousel anf build update

--- src/App.tsx
+++ src/App.tsx
@@ -5,46 +5,15 @@ import Crousel from './crousel/Crousel';
 function App() {
   return (
     <div className="App">
-    <Nav></Nav> 
-    <Crousel></Crousel>
-
- <div className="container  d-flex " >
-<div className="col-md-6 ">
-      <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-        <div className="col p-4 d-flex flex-column position-static">
-          <strong className="d-inline-block mb-2 text-primary">World</strong>
-          <h3 className="mb-0">iCoder offices</h3>
-          <div className="mb-1 text-muted">Nov 12</div>
-          <p className="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" className="stretched-link">Continue reading</a>
-        </div>
-        <div className="col-auto d-none d-lg-block">
-       <img src="https://source.unsplash.com/200x250/?glass,gogle,code" className="bd-placeholder-img" width="200" height="250" alt="..."/>
-
-        </div>
+      <Nav></Nav> 
+      <Crousel></Crousel>
+      <div className="card">
+        <h2> Welcome to SRIST space </h2>
+        <br />
+        A Online community where students of srist can post notes, host events, ask help, poll.
+        <br />
+        Have Fun! 
       </div>
-    </div>
-<div className="col-md-6 ">
-      <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-        <div className="col p-4 d-flex flex-column position-static">
-          <strong className="d-inline-block mb-2 text-primary">World</strong>
-          <h3 className="mb-0">iCoder offices</h3>
-          <div className="mb-1 text-muted">Nov 12</div>
-          <p className="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" className="stretched-link">Continue reading</a>
-        </div>
-        <div className="col-auto d-none d-lg-block">
-       <img src="https://source.unsplash.com/200x250/?tech,earphones,code" className="bd-placeholder-img" width="200" height="250" alt="..."/>
-        </div>
-      </div>
-    </div>
-</div>
-<footer className="container">
-    <p className="float-end"><a href="#">Back to top</a></p>
-    <p>© 2020–2021 iCoder, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
-  </footer>
-
-
     </div>
   );
 }
--- src/crousel/Crousel.tsx
+++ src/crousel/Crousel.tsx
@@ -2,32 +2,32 @@ import "./Crousel.css";
 
 function Crousel() {
     return(
-        <>
-        <div className="showslider middle">
-        <div className="slides">
+       <>
+        <div className="showslider">
+          <div className="slides">
        
-<input type="radio" name="a" id="a1" checked/>
-<input type="radio" name="a" id="a2" checked />
-<input type="radio" name="a" id="a3" checked />
+           <input type="radio" name="a" id="a1" checked/>
+           <input type="radio" name="a" id="a2" checked />
+           <input type="radio" name="a" id="a3" checked />
 
-        <div className="slide s1">
-      <img src="https://educationworld4u.com/images/college/1975992703-shri-ram-institute-of-technology-jabalpur-city-jabalpur-colleges-tthlike.jpg" className="d-block w-100" width="1200px" height="500px" alt="..."/>
-      </div>
-        <div className="slide">
-   <img src="https://images.unsplash.com/photo-1587614382346-4ec70e388b28?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" className="d-block w-100"  width="1200" height="500" alt="..."/>
-      </div>
-        <div className="slide">
-      <img src="https://images.unsplash.com/photo-1587614382346-4ec70e388b28?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" width="1200" height="500" className="d-block w-100" alt="..."/>
-      </div>
-        </div>
+             <div className="slide s1">
+               <img src="https://educationworld4u.com/images/college/1975992703-shri-ram-institute-of-technology-jabalpur-city-jabalpur-colleges-tthlike.jpg" className="d-block w-100" width="1200px" height="500px" alt="..."/>
+             </div>
+             <div className="slide">
+               <img src="https://images.unsplash.com/photo-1587614382346-4ec70e388b28?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" className="d-block w-100"  width="1200" height="500" alt="..."/>
+              </div>
+              <div className="slide">
+                <img src="https://images.unsplash.com/photo-1587614382346-4ec70e388b28?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" width="1200" height="500" className="d-block w-100" alt="..."/>
+              </div>
+           </div>
          
         <div className="navigation">
-        <label htmlFor="a1" className="bar"></label>
-        <label htmlFor="a2" className="bar"></label>
-        <label htmlFor="a3" className="bar"></label>
+            <label htmlFor="a1" className="bar"></label>
+            <label htmlFor="a2" className="bar"></label>
+            <label htmlFor="a3" className="bar"></label>
+          </div>
         </div>
-        </div>
-      </> 
+       </> 
         );
     }
     export default Crousel;
