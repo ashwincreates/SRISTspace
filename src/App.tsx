d7103dc73fa53d202cd077ba5c42e7c91e50a73d
Bsandeep7 <baghel7970@gmail.com>
2021-07-25 16:44:43 +0530

    article and event added

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
-import './App.css';
-import Nav from './nav/Nav';
-import Crousel from './crousel/Crousel';
+import "./App.css";
+import Nav from "./nav/Nav";
+import Crousel from "./crousel/Crousel";
 import List from "./note/noteList";
-
+import Article from "./forArticle/Article";
+import Event from "./forevent/Event";
 function App() {
   return (
     <>
-    <div className="App">
-      <Nav></Nav> 
-      <Crousel></Crousel>
-      <div className="card">
-        <h2> Welcome to SRIST space </h2>
-        <br />
-        A Online community where students of srist can post notes, host events, ask help, poll.
-        <br />
-        Have Fun! 
+      <div className="App">
+        <Nav></Nav>
+        <Crousel></Crousel>
+        <div className="card">
+          <h2> Welcome to SRIST space </h2>
+          <br />
+          A Online community where students of srist can post notes, host
+          events, ask help, poll.
+          <br />
+          Have Fun !!
+        </div>
+        <List></List>
+        <Article></Article>
+        <br></br>
+        <Event></Event>
       </div>
-     <List></List>
-    </div>
     </>
   );
 }
