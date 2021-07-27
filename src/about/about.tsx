66b88e4245bec3d6efa2ab43c30d625a57af25fa
Bsandeep7 <baghel7970@gmail.com>
2021-07-27 23:45:33 +0530

    navabout update

--- src/about/about.tsx
+++ src/about/about.tsx
@@ -1,8 +1,155 @@
+import "./about.css";
 function About() {
-    return(
-        <>
-         This is About page
-        </>
-    );
+  return (
+    <>
+      <div className="about1">
+        <h2>
+          We are students of SRIST Computer Science Stream and We wanted to
+          bring our College experience on an online plateform Lorem, ipsum dolor
+          sit amet consectetur adipisicing elit. Accusantium error voluptas
+          praesentium optio odio qui architecto blanditiis similique incidunt
+          quam. Modi ullam explicabo obcaecati et quaerat natus deleniti dolore
+          placeat.
+        </h2>
+      </div>
+      <h2 className="about2">Contributer</h2>
+      <div className="about5">
+        <div className="about3">
+          <div>
+            {" "}
+            <img
+              className="about4"
+              src="https://pbs.twimg.com/media/Dkfak-NUcAIhYE-.jpg"
+              alt="dev img"
+            ></img>
+          </div>
+          <h2>
+            <span className="name">Ashwin </span>
+            <br></br>
+
+            <span className="role">Backend and frontend</span>
+            <br></br>
+
+            <span className="section">Section-A</span>
+          </h2>
+              </div>
+              <div className="about3">
+          <div>
+            {" "}
+            <img
+              className="about4"
+              src="https://pbs.twimg.com/media/Dkfak-NUcAIhYE-.jpg"
+              alt="dev img"
+            ></img>
+          </div>
+          <h2>
+            <span className="name">Name </span>
+            <br></br>
+
+            <span className="role">Role</span>
+            <br></br>
+
+            <span className="section">Class section</span>
+          </h2>
+              </div>
+              <div className="about3">
+          <div>
+            {" "}
+            <img
+              className="about4"
+              src="https://pbs.twimg.com/media/Dkfak-NUcAIhYE-.jpg"
+              alt="dev img"
+            ></img>
+          </div>
+          <h2>
+            <span className="name">Name </span>
+            <br></br>
+
+            <span className="role">Role</span>
+            <br></br>
+
+            <span className="section">Class section</span>
+          </h2>
+              </div>
+              <div className="about3">
+          <div>
+            {" "}
+            <img
+              className="about4"
+              src="https://pbs.twimg.com/media/Dkfak-NUcAIhYE-.jpg"
+              alt="dev img"
+            ></img>
+          </div>
+          <h2>
+            <span className="name">Name </span>
+            <br></br>
+
+            <span className="role">Role</span>
+            <br></br>
+
+            <span className="section">Class section</span>
+          </h2>
+              </div>
+      </div>
+      <div className="about5">
+        <div className="about3">
+          <div>
+            {" "}
+            <img
+              className="about4"
+              src="https://pbs.twimg.com/media/Dkfak-NUcAIhYE-.jpg"
+              alt="dev img"
+            ></img>
+          </div>
+          <h2>
+            <span className="name">Name </span>
+            <br></br>
+
+            <span className="role">Role</span>
+            <br></br>
+
+            <span className="section">Class section</span>
+          </h2>
+        </div>
+
+        <div className="about3">
+          <div>
+            {" "}
+            <img
+              className="about4"
+              src="https://pbs.twimg.com/media/Dkfak-NUcAIhYE-.jpg"
+              alt="dev img"
+            ></img>
+          </div>
+          <h2>
+            <span className="name">Name </span>
+            <br></br>
+            <span className="role">Role</span>
+            <br></br>
+            <span className="section">Class section</span>
+          </h2>
+              </div>
+              <div className="about3">
+          <div>
+            {" "}
+            <img
+              className="about4"
+              src="https://pbs.twimg.com/media/Dkfak-NUcAIhYE-.jpg"
+              alt="dev img"
+            ></img>
+          </div>
+          <h2>
+            <span className="name">Name </span>
+            <br></br>
+
+            <span className="role">Role</span>
+            <br></br>
+
+            <span className="section">Class section</span>
+          </h2>
+              </div>
+      </div>
+    </>
+  );
 }
 export default About;
