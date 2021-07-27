c79d6ec92fb5b020a4ce3236dd752220935947af
Bsandeep7 <baghel7970@gmail.com>
2021-07-27 16:33:23 +0530

    navevent update

--- src/events/event.tsx
+++ src/events/event.tsx
@@ -1,9 +1,109 @@
+import "../notes/note.css";
+import "./event.css";
 function Event() {
-  return(
+  return (
     <>
-      This is Events
+      <div className="card notes">
+        <input
+          className="searchbar"
+          name="search"
+          type="text"
+          placeholder="search here"
+        />
+        <h2 className="heading"> Event</h2>
+
+        <br />
+        <div className="content">
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
+        </div>
+        <br />
+
+        <button className="explore">Host A Event &#62;</button>
+      </div>
+      <div className="selection">
+        <h2 className="subject">Coming Up This Week</h2>
+      </div>
+      <div className="eventweek">
+        <div className="eventname"></div>
+        <h2 className="eventheading">
+          Event name<span>100k </span>
+        </h2>
+        <p className="eventheading">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis impedit
+          repellat error deserunt doloremque, praesentium provident cumque sit?
+          Amet eos praesentium quam accusantium dolorum nam eaque soluta
+          recusandae reiciendis asperiores.
+        </p>
+      </div>
+      <div className="eventweek">
+        <div className="eventname"></div>
+        <h2 className="eventheading">
+          Event name<span>100k </span>
+        </h2>
+        <p className="eventheading">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis impedit
+          repellat error deserunt doloremque, praesentium provident cumque sit?
+          Amet eos praesentium quam accusantium dolorum nam eaque soluta
+          recusandae reiciendis asperiores.
+        </p>
+      </div>
+      <div className="eventweek">
+        <div className="eventname"></div>
+        <h2 className="eventheading">
+          Event name<span>100k </span>
+        </h2>
+        <p className="eventheading">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis impedit
+          repellat error deserunt doloremque, praesentium provident cumque sit?
+          Amet eos praesentium quam accusantium dolorum nam eaque soluta
+          recusandae reiciendis asperiores.
+        </p>
+      </div>
+      <br></br>
+      <hr></hr>
+      <div className="eventweek">
+        <div className="eventname"></div>
+        <h2 className="eventheading">
+          Event name<span>100k </span>
+        </h2>
+        <p className="eventheading">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis impedit
+          repellat error deserunt doloremque, praesentium provident cumque sit?
+          Amet eos praesentium quam accusantium dolorum nam eaque soluta
+          recusandae reiciendis asperiores.
+        </p>
+      </div>
+      <div className="eventweek">
+        <div className="eventname"></div>
+        <h2 className="eventheading">
+          Event name<span>100k </span>
+        </h2>
+        <p className="eventheading">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis impedit
+          repellat error deserunt doloremque, praesentium provident cumque sit?
+          Amet eos praesentium quam accusantium dolorum nam eaque soluta
+          recusandae reiciendis asperiores.
+        </p>
+      </div>
+      <div className="eventweek">
+        <div className="eventname"></div>
+        <h2 className="eventheading">
+          Event name<span>100k </span>
+        </h2>
+        <p className="eventheading">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis impedit
+          repellat error deserunt doloremque, praesentium provident cumque sit?
+          Amet eos praesentium quam accusantium dolorum nam eaque soluta
+          recusandae reiciendis asperiores.
+        </p>
+      </div>
     </>
-  )
+  );
 }
 
 export default Event;
