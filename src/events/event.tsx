304f2af1706acd06f84af337c0f066b52086e117
Bsandeep7 <baghel7970@gmail.com>
2021-07-28 20:32:03 +0530

    ...

--- src/events/event.tsx
+++ src/events/event.tsx
@@ -64,8 +64,32 @@ function Event() {
           recusandae reiciendis asperiores.
         </p>
       </div>
-      <br></br>
+      <div className="eventweek">
+        <div className="eventname"></div>
+        <h2 className="eventheading">
+          Event name<span>100k </span>
+        </h2>
+        <p className="eventheading">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis impedit
+          repellat error deserunt doloremque, praesentium provident cumque sit?
+          Amet eos praesentium quam accusantium dolorum nam eaque soluta
+          recusandae reiciendis asperiores.
+        </p>
+      </div>
+      
      
+      <div className="eventweek">
+        <div className="eventname"></div>
+        <h2 className="eventheading">
+          Event name<span>100k </span>
+        </h2>
+        <p className="eventheading">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis impedit
+          repellat error deserunt doloremque, praesentium provident cumque sit?
+          Amet eos praesentium quam accusantium dolorum nam eaque soluta
+          recusandae reiciendis asperiores.
+        </p>
+      </div>
       <div className="eventweek">
         <div className="eventname"></div>
         <h2 className="eventheading">
