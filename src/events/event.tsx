a4d83f45104279b6e02383a96574a3d77b5fc9ea
Ayush <abhay27nigam02@gmail.com>
2021-07-28 23:15:48 +0530

    responsivetest

--- src/events/event.tsx
+++ src/events/event.tsx
@@ -3,35 +3,32 @@ import "./event.css";
 function Event() {
   return (
     <>
-      <div className="card notes">
-        <input
-          className="searchbar"
-          name="search"
-          type="text"
-          placeholder="search here"
-        />
-        <h2 className="heading"> Event</h2>
-
-        <br />
+     <div className="header">
         <div className="content">
+          <h3 className="title-text"> Article </h3>  
           <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
             ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
+            and scrambled it to make a type specimen book. 
           </p>
+          <button className="explore">Explore &#62;</button>
         </div>
-        <br />
-
-        <button className="explore">Host A Event &#62;</button>
+        <input
+            className="search"
+            name="search"
+            type="text"
+            placeholder="search here"
+        />
       </div>
       <div className="selection">
         <h2 className="subject">Coming Up This Week</h2>
       </div>
+      <div className="event">
       <div className="eventweek">
         <div className="eventname"></div>
         <h2 className="eventheading">
-          Event name<span>100k </span>
+          Event name1<span>100k </span>
         </h2>
         <p className="eventheading">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis impedit
@@ -43,7 +40,7 @@ function Event() {
       <div className="eventweek">
         <div className="eventname"></div>
         <h2 className="eventheading">
-          Event name<span>100k </span>
+          Event name2<span>100k </span>
         </h2>
         <p className="eventheading">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis impedit
@@ -55,21 +52,7 @@ function Event() {
       <div className="eventweek">
         <div className="eventname"></div>
         <h2 className="eventheading">
-          Event name<span>100k </span>
-        </h2>
-        <p className="eventheading">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis impedit
-          repellat error deserunt doloremque, praesentium provident cumque sit?
-          Amet eos praesentium quam accusantium dolorum nam eaque soluta
-          recusandae reiciendis asperiores.
-        </p>
-      </div>
-      <br></br>
-     
-      <div className="eventweek">
-        <div className="eventname"></div>
-        <h2 className="eventheading">
-          Event name<span>100k </span>
+          Event name3<span>100k </span>
         </h2>
         <p className="eventheading">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis impedit
@@ -81,7 +64,7 @@ function Event() {
       <div className="eventweek">
         <div className="eventname"></div>
         <h2 className="eventheading">
-          Event name<span>100k </span>
+          Event name4<span>100k </span>
         </h2>
         <p className="eventheading">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis impedit
@@ -93,7 +76,7 @@ function Event() {
       <div className="eventweek">
         <div className="eventname"></div>
         <h2 className="eventheading">
-          Event name<span>100k </span>
+          Event name5<span>100k </span>
         </h2>
         <p className="eventheading">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis impedit
@@ -101,6 +84,19 @@ function Event() {
           Amet eos praesentium quam accusantium dolorum nam eaque soluta
           recusandae reiciendis asperiores.
         </p>
+      </div>
+      <div className="eventweek">
+        <div className="eventname"></div>
+        <h2 className="eventheading">
+          Event name6<span>100k </span>
+        </h2>
+        <p className="eventheading">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis impedit
+          repellat error deserunt doloremque, praesentium provident cumque sit?
+          Amet eos praesentium quam accusantium dolorum nam eaque soluta
+          recusandae reiciendis asperiores.
+        </p>
+      </div>
       </div>
     </>
   );
--- src/notes/notes.tsx
+++ src/notes/notes.tsx
@@ -2,27 +2,23 @@ import "./note.css";
 function Notes() {
   return (
     <>
-      <div className="card notes">
-        <h2 className="heading"> Notes </h2>
-      <input
-          className="searchbar"
-          name="search"
-          type="text"
-          placeholder="search here"
-        />
-        
-        <br />
+     <div className="header">
         <div className="content">
+          <h3 className="title-text"> Notes </h3>  
           <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
             ever since the 1500s, when an unknown printer took a galley of type
             and scrambled it to make a type specimen book. 
           </p>
+          <button className="explore">Explore &#62;</button>
         </div>
-        <br />
-      
-        <button className="explore">Explore &#62;</button>
+        <input
+            className="search"
+            name="search"
+            type="text"
+            placeholder="search here"
+        />
       </div>
       <div className="selection">
         <h2 className="subject">Subjects</h2>
@@ -51,62 +47,62 @@ function Notes() {
           </select>
         </form>
         <br />
-        <div className="tray list">
-          <div className="note-card grid">
-            <div>
-              <h3>Card 1</h3>
-              <span>
-                Some text
-                <br />
-              </span>
-            </div>
-          </div>
-          <div className="note-card grid">
-            <div>
-              <h3>Card 3</h3>
-              <span>
-                Some text
-                <br />
-              </span>
-            </div>
-          </div>
-          <div className="note-card grid">
-            <div>
-              <h3>Card 3</h3>
-              <span>
-                Some text
-                <br />
-              </span>
-            </div>
-          </div>
-          <div className="note-card grid">
-            <div>
-              <h3>Card 4</h3>
-              <span>
-                Some text
-                <br />
-              </span>
-            </div>
-          </div>
-          <div className="note-card grid">
-            <div>
-              <h3>Card 6</h3>
-              <span>
-                Some text
-                <br />
-              </span>
-            </div>
-          </div>
-          <div className="note-card grid">
-            <div>
-              <h3>Card 5</h3>
-              <span>
-                Some text
-                <br />
-              </span>
-            </div>
+       <div className="item-tray">
+        <div className="card-md">
+          <div>
+            <h3>Card 1</h3>
+            <span>
+              Some text
+              <br />
+            </span>
           </div>
         </div>
+        <div className="card-md">
+          <div>
+            <h3>Card 1</h3>
+            <span>
+              Some text
+              <br />
+            </span>
+          </div>
+        </div>
+        <div className="card-md">
+          <div>
+            <h3>Card 1</h3>
+            <span>
+              Some text
+              <br />
+            </span>
+          </div>
+        </div>
+        <div className="card-md">
+          <div>
+            <h3>Card 1</h3>
+            <span>
+              Some text
+              <br />
+            </span>
+          </div>
+        </div>
+        <div className="card-md">
+          <div>
+            <h3>Card 1</h3>
+            <span>
+              Some text
+              <br />
+            </span>
+          </div>
+        </div>
+        <div className="card-md">
+          <div>
+            <h3>Card 1</h3>
+            <span>
+              Some text
+              <br />
+            </span>
+          </div>
+        </div>
+      </div>
       </div>
       <div className="head">
         <div className="section-title">
