5648e46330cfab20141a9a52c1496af15b47cf99
ashwin <ashwin@pop-os.localdomain>
2021-07-29 15:29:25 +0530

    postponed

--- src/auth/auth.tsx
+++ src/auth/auth.tsx
@@ -1,7 +1,28 @@
-function signup() {
+import './auth.css';
+
+function submit() {
+  console.log("submitted");
+}
+
+function Auth() {
   return (
-    <div></div>  
+    <div className="center">
+      <div className="box">
+         <div className="auth" onSubmit={() => submit()}>
+           <h2>Sign up</h2>
+           <form>
+           <input className="search text" placeholder="email"/>
+           <input className="search text" placeholder="semester"/>
+           <input className="search text" placeholder="stream"/>
+           <input className="search text" placeholder="branch"/>
+           <input className="search text" placeholder="password"/>
+           <br />
+           <button>Sign Up</button>
+           </form> 
+         </div>
+      </div>
+    </div>
   );
 }
 
-export default signup;
+export default Auth;
