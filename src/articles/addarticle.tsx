eaf889863595e56327fd8091112e7d79148b0cc9
ashwin <ashwin@pop-os.localdomain>
2021-08-10 15:42:54 +0530

    placeholder update

--- src/articles/addarticle.tsx
+++ src/articles/addarticle.tsx
@@ -2,81 +2,65 @@ import React from "react";
 import Icons from "../icons/icons";
 
 interface State {
-	title : string;
-	content : any;
-	isEmptycontent : boolean;
-	isEmptytitle : boolean;
+  title: string;
+  content: any;
 }
 
-class AddArticle extends React.Component<{}, State>{
-	constructor(props : any){
-		super(props);
-		this.state = {
-			title : "Your Title Here...",
-			content : "Type body here...",
-			isEmptycontent : true,
-			isEmptytitle : true,
-		}
-		this.focusLost = this.focusLost.bind(this)
-		this.toggletitle = this.toggletitle.bind(this)
-		this.togglecontent = this.togglecontent.bind(this)
-	}
-
-	componentDidUpdate(){
-		const heading = document.getElementById("heading");
-		const content = document.getElementById("content");
-		if(heading?.innerHTML === "<br>" || heading?.innerHTML === "")
-		{
-			heading?.focus();
-		}
-
-		if(content?.innerHTML === "<br>" || content?.innerHTML === "") {
-			content?.focus();
-		}
-	}
+class AddArticle extends React.Component<{}, State> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      title: "Your Title Here...",
+      content: "Type body here...",
+    };
+    this.focusLost = this.focusLost.bind(this);
+  }
 	
-	printArticle(){
-		const heading = document.getElementById("heading")?.innerHTML;
-		const content = document.getElementById("content")?.innerHTML;
-		console.log(`${heading}\n${content}`);
-	}
 
-	focusLost() {
-		const heading = document.getElementById("heading");
-		const content = document.getElementById("content");
-		if(heading?.innerHTML === "<br>" || heading?.innerHTML === "")
-		{
-			this.setState({isEmptytitle : true})
-		}
+  focusLost() {
+    const heading = document.getElementById("heading");
+    const content = document.getElementById("content");
+    if (heading?.innerHTML === "<br>" || heading?.innerHTML === "") {
+      if (heading) heading.innerHTML = "";
+    }
 
-		if(content?.innerHTML === "<br>" || content?.innerHTML === "") {
-			this.setState({isEmptycontent : true})
-		}
-	}
-	
-	toggletitle(){
-		this.setState({isEmptytitle : false})
-	}
+    if (
+      content?.children[0].innerHTML === "<br>" ||
+      content?.children[0].innerHTML === ""
+    ) {
+      if (content?.children[0]) content.children[0].innerHTML = "";
+    }
+  }
 
-	togglecontent(){
-		this.setState({isEmptycontent: false})
-	}
-
-	render() {
-		
-		return (
-			<>
-				<div className="menu">
-				<button onClick={() => this.printArticle()}>Publish article</button>
-				</div>
-				<div className="container">
-			 	{this.state.isEmptytitle?<p className="no-text-title" onClick={this.toggletitle}>Your Title here...</p>:<p contentEditable id="heading" className="article-heading" onBlur={() => this.focusLost()}></p>}
-			 	{this.state.isEmptycontent?<p className="no-text-content" onClick={this.togglecontent}>Type content here...</p>:<p contentEditable id="content" onBlur={this.focusLost} className="article-content"></p>}
-				</div>
-				<button className="icon-button explore publish"><Icons name="publish"></Icons></button>
-			</>
-		)
-	}	
+  render() {
+    return (
+      <>
+        <div className="menu">
+          <button onClick={() => {}}>Publish article</button>
+        </div>
+        <div className="container">
+          <p
+            contentEditable
+            placeholder="Title goes here.."
+            id="heading"
+            className="article-heading"
+            onBlur={() => this.focusLost()}
+          ></p>
+          <article id="content">
+            <p
+              contentEditable
+              placeholder={"The content goes here"}
+              onBlur={this.focusLost}
+              className="article-content"
+            ></p>
+          </article>
+        </div>
+        <button className="icon-button explore publish">
+          <Icons name="publish"></Icons>
+        </button>
+      </>
+    );
+  }
 }
 
 export default AddArticle;
