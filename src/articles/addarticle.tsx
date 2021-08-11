16b30a682157ed40887db8658ecfe77fc7fac0c8
ashwin <ashwin@pop-os.localdomain>
2021-08-11 10:39:10 +0530

    placeholder fix

--- src/articles/addarticle.tsx
+++ src/articles/addarticle.tsx
@@ -14,22 +14,23 @@ class AddArticle extends React.Component<{}, State> {
       content: "Type body here...",
     };
     this.focusLost = this.focusLost.bind(this);
-  }
+}
+
+	handlekeydown(event : any){
+		switch(event.keyCode){
+			case 8 : let parentele = event.target.parentNode;if(event.target.parentNode.children.length > 1 && event.target.innerHTML === "<br>"){event.target.outerHTML = "";parentele.children[parentele.children.length - 1].focus()};break;
+			case 13 : let p = document.createElement("p");p.contentEditable="true";p.onblur=this.focusLost;p.className="article-content";p.setAttribute("placeholder", "The content goes here");event.target.parentNode.insertBefore(p, event.target.nextSibling);p.focus();break;
+			default : break;
+		}
+	}
 	
+	componentDidMount(){
+		document.getElementById("content")?.addEventListener("keydown", this.handlekeydown)
+	}
 
-  focusLost() {
-    const heading = document.getElementById("heading");
-    const content = document.getElementById("content");
-    if (heading?.innerHTML === "<br>" || heading?.innerHTML === "") {
-      if (heading) heading.innerHTML = "";
-    }
-
-    if (
-      content?.children[0].innerHTML === "<br>" ||
-      content?.children[0].innerHTML === ""
-    ) {
-      if (content?.children[0]) content.children[0].innerHTML = "";
-    }
+  focusLost(event : any) {
+	if(event.target.innerHTML === "<br>")event.target.innerHTML="";
+	console.log("data cleaned")
   }
 
   render() {
@@ -44,7 +45,7 @@ class AddArticle extends React.Component<{}, State> {
             placeholder="Title goes here.."
             id="heading"
             className="article-heading"
-            onBlur={() => this.focusLost()}
+            onBlur={this.focusLost}
           ></p>
           <article id="content">
             <p
