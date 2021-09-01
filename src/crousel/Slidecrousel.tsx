00297ad19a728b1d72aef2825b78f87dd9e3a0b9
utkarsh choudhary <utkarshchoudhary80@gmail.com>
2021-09-01 20:37:58 +0530

    carousel

--- src/crousel/Slidecrousel.tsx
+++ src/crousel/Slidecrousel.tsx
@@ -11,12 +11,16 @@ interface slideStates {
   index: number;
   items: Array<slides>;
   refArr: Array<Object>;
+  scrollable:boolean;
 }
 
-interface props {}
+interface props {
+scrollable:boolean;
+
+}
 
 export default class Slidecrousel extends React.Component<props, slideStates> {
-  divRef = [React.createRef<HTMLElement>(), React.createRef<HTMLElement>()];
+  divRef = [React.createRef<HTMLElement>(), React.createRef<HTMLElement>() , React.createRef<HTMLElement>()];
 
   constructor(props: any) {
     super(props);
@@ -29,7 +33,9 @@ export default class Slidecrousel extends React.Component<props, slideStates> {
         {
           id: "1",
           img: "https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80",
-        },
+        },{
+          id:"2" , img : "https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        }
       ],
       index: 0,
       images: [
@@ -37,15 +43,71 @@ export default class Slidecrousel extends React.Component<props, slideStates> {
         "https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80",
       ],
       refArr: [],
+      scrollable:true
     };
+
+    this.changeImageForward = this.changeImageForward.bind(this);
+    this.changeImageBackWard = this.changeImageBackWard.bind(this);
   }
 
-  changeImage(id) {
-    this.divRef[id].current?.scrollIntoView();
+  changeImageForward() {
+    if (this.state.index < this.state.items.length - 1 ){
+      var a = this.state.index+1 
+      this.divRef[a].current?.scrollIntoView();
+      this.setState({index:a})
+    }
+  
   }
 
+  changeImageBackWard(){
+
+    if (this.state.index < this.state.items.length && this.state.index > 0){
+      var a = this.state.index-1
+      this.divRef[a].current?.scrollIntoView();
+      this.setState({index:a})
+    }
+    
+  }
+
+  checkScrollable(){
+    this.setState({scrollable:false})
+    alert("scroll")
+  }
+
+  componentDidMount(){
+const interval = setInterval(()=>{
+      if (this.state.index < this.state.items.length - 1){
+        var intRef = this.state.index+1;
+        var stringRef = intRef+"";
+        this.divRef[stringRef].current.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' })
+        this.setState({index:intRef})
+      }else {
+this.setState({index:0})
+this.divRef[this.state.index+""].current.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' })
+      }
+} , 3000)
+
+document.body.addEventListener("scroll" , this.checkScrollable ,false)
+
+if (!this.props.scrollable){
+  clearInterval(interval)
+}
+
+
+
+}
+
+componentWillUnmount(){
+  this.setState({scrollable:false})
+}
+
+
+   
   render() {
+
     return (
+      <div className = "super">
+     
       <div className="fragment">
         {/* <img
 alt = "" 
@@ -58,9 +120,24 @@ onTransitionEnd = {this.changeImage}
           </div>
         ))}
 
-        <div className="upperLayer">
-          <button onClick={this.changeImage} />
+
+      </div>
+     
+        <div className = "forward">
+          <img style = {{
+            height:"50px",
+            width:"50px"
+          }} src = {process.env.PUBLIC_URL + "/arrowr.png"} alt = "" onClick={this.changeImageForward} />
         </div>
+
+        <div className = "backward">
+<img style = {{
+  height:"50px" ,
+  width:"50px"
+}} alt = "" src = {process.env.PUBLIC_URL + "/arrow.png"} onClick = {this.changeImageBackWard}/>
+
+        </div>
+       
       </div>
     );
   }
